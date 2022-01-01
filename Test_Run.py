1d5f229a861c0b30dc962a921e7280858f5cc6ac
Cody Zhang <codyzhang4@hotmail.com>
2022-01-01 01:00:49 -0800

    added functionality for independent courses and began decomposition

--- Test_Run.py
+++ Test_Run.py
@@ -18,7 +18,6 @@ encouragements = [
 current_date = datetime.datetime.now()
 # convert the date to a readable format with only relevant information
 printable_date = current_date.strftime("%m-%d")
-print(printable_date)
 
 # location of the excel file
 file = 'DiscordExcel.xlsx'
@@ -30,7 +29,27 @@ sheet = wb.active
 
 @client.event
 async def on_ready():
+    # message to show that the bot is logged in (connected properly)
     print('We have logged in as {0.user}'.format(client))
+
+
+# Messageable.send has to be awaited while using discord
+# function has to be asynchronous to use await
+async def date_comparison(message):
+    for rows in range(2, sheet.max_row + 1):
+        # just reading the date here
+        for columns in range(4, 5):
+            print((sheet.cell(rows, 4).value[1:3]))
+            cell_value = sheet.cell(row=rows, column=columns)
+            # iteration starts at 1 with openpyxl, we are comparing the same indices(month)
+            # if due-month > current month or due-day > current-day given the same month
+            if int(cell_value.value[1:3]) > int(printable_date[0:2]) or int(cell_value.value[1:3]) == \
+                    int(printable_date[0:2] and int(cell_value.value[4:6]) > int(printable_date[3:5])):
+                await message.channel.send(f"{sheet.cell(rows, 1).value} {sheet.cell(rows, 2).value} "
+                                           f"{sheet.cell(rows, 3).value}  Difficulty: {sheet.cell(rows, 5).value}")
+                # excel auto formats to non-numerical date without 1 space, so I'm stripping the space
+                # could git rid of the strip() if someone knows how to properly format excel dates without auto-conversion
+                await message.channel.send(f'Due: {cell_value.value.strip()}')
 
 
 @client.event
@@ -38,21 +57,37 @@ async def on_message(message):
     # # what does this do? is it necessary?
     # if message.author == client.user:
     #     return
+    if message.content.lower() == "deadlines" or message.content.lower() == "due".lower():
+        await date_comparison(message)
+        # for rows in range(2, sheet.max_row + 1):
+        #     # just reading the date here
+        #     for columns in range(4, 5):
+        #         cell_value = sheet.cell(row=rows, column=columns)
+        #         # iteration starts at 1 with openpyxl, we are comparing the same indices(month)
+        #         # if due-month > current month or due-day > current-day given the same month
+        #         if int(cell_value.value[1:3]) > int(printable_date[0:2]) or int(cell_value.value[1:3]) == \
+        #                 int(printable_date[0:2] and int(cell_value.value[4:6]) > int(printable_date[3:5])):
+        #             await message.channel.send(f"{sheet.cell(rows, 1).value} {sheet.cell(rows, 2).value} "
+        #                                        f"{sheet.cell(rows, 3).value}  Difficulty: {sheet.cell(rows, 5).value}")
+        #             # excel auto formats to non-numerical date without 1 space, so I'm stripping the space
+        #             # could git rid of the strip() if someone knows how to properly format excel dates without auto-conversion
+        #             await message.channel.send(f'Due: {cell_value.value.strip()}')
+        await message.channel.send(random.choice(encouragements))
 
-    if message.content == "deadlines" or message.content == "due":
+    # AttributeError: 'builtin_function_or_method' object has no attribute 'lower'
+    # second part of conditional checks for a specific course
+    elif message.content.startswith("deadlines") and len(message.content) == 14:
         for rows in range(2, sheet.max_row + 1):
-            # just reading the date here
-            for columns in range(4, 5):
+            for columns in range(2, 3):
                 cell_value = sheet.cell(row=rows, column=columns)
-                # iteration starts at 1 with openpyxl, we are comparing the same indices(month)
-                # if due-month > current month or due-day > current-day given the same month
-                if int(cell_value.value[1:3]) > int(printable_date[0:2]) or int(cell_value.value[1:3]) == \
-                        int(printable_date[0:2] and int(cell_value.value[4:6]) > int(printable_date[3:5])):
-                    await message.channel.send(f"{sheet.cell(rows, 1).value} {sheet.cell(rows, 2).value} "
-                                               f"{sheet.cell(rows, 3).value}  Difficulty: {sheet.cell(rows, 5).value}")
-                    # excel auto formats to non-numerical date without 1 space, so I'm stripping the space
-                    # could git rid of the strip() if someone knows how to properly format excel dates without auto-conversion
-                    await message.channel.send(f'Due: {cell_value.value.strip()}')
+                if str(cell_value.value) == (message.content[10:]):
+                    # this is a datechecker again, can probably be universally broken down into a helper somehow
+                    if int(sheet.cell(rows, 4).value[1:3]) > int(printable_date[0:2]) or \
+                            int(sheet.cell(rows, 4).value[1:3]) == int(printable_date[0:2]) and \
+                            int(sheet.cell(rows, 4).value[4:6]) > int(printable_date[3:5]):
+                        await message.channel.send(f"{sheet.cell(rows, 1).value} {sheet.cell(rows, 2).value} "
+                                                   f"{sheet.cell(rows, 3).value}  Difficulty: {sheet.cell(rows, 5).value}")
+                        await message.channel.send(f'Due: {sheet.cell(rows, 4).value.strip()}')
         await message.channel.send(random.choice(encouragements))
 
 load_dotenv()
